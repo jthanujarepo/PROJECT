c8a6267b45f5e35598c63bcf5536a0044c2eaffd
Thanuja <jthanuja4@gmail.com>
2026-01-10 22:09:28 +0530

    Prepare project for Render deployment

--- Demo/settings.py
+++ Demo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h_dx0f(x3%5d54d__j%#*p(rj1jxsjiexh7yt5tg)lcfw!3mtj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False         #########
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',     ############
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,8 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # MEDIA_URL='/images/'
-MEDIA_ROOT=BASE_DIR/'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_ROOT=BASE_DIR/'static'
 
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,"static")
